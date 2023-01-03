41fe474e78d4729e18dacd277c782ec463d8ee99
Rongzhong Li <rzlib2l@gmail.com>
2023-01-03 15:21:12 +0800

    add warning if the IMU stucks

--- src/imu.h
+++ src/imu.h
@@ -128,22 +128,22 @@ MPU6050 mpu;
 //#define OUTPUT_TEAPOT
 
 // MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
+bool dmpReady = false;   // set true if DMP init was successful
+uint8_t mpuIntStatus;    // holds actual interrupt status byte from MPU
+uint8_t devStatus;       // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;     // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;      // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64];  // FIFO storage buffer
 
 // orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 gy;         // [x, y, z]            gyro sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector. unit is radian
+Quaternion q;         // [w, x, y, z]         quaternion container
+VectorInt16 aa;       // [x, y, z]            accel sensor measurements
+VectorInt16 gy;       // [x, y, z]            gyro sensor measurements
+VectorInt16 aaReal;   // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;  // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;  // [x, y, z]            gravity vector
+float euler[3];       // [psi, theta, phi]    Euler angle container
+float ypr[3];         // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector. unit is radian
 int8_t yprTilt[3];
 
 float originalYawDirection;
@@ -156,7 +156,7 @@ uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r'
 // ===               INTERRUPT DETECTION ROUTINE                ===
 // ================================================================
 
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+volatile bool mpuInterrupt = false;  // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
   mpuInterrupt = true;
 }
@@ -236,14 +236,14 @@ void print6Axis() {
   Serial.print("\t");
 #endif
   Serial.print("areal.z\t");
-  Serial.print(aaReal.z); //becomes negative when flipped
+  Serial.print(aaReal.z);  //becomes negative when flipped
   Serial.print("\t");
 
   Serial.println();
 }
 
 bool read_IMU() {
-  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {  // Get the Latest packet
     // display Euler angles in degrees
     mpu.dmpGetQuaternion(&q, fifoBuffer);
     mpu.dmpGetAccel(&aa, fifoBuffer);
@@ -253,7 +253,7 @@ bool read_IMU() {
     mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
     mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
 
-    for (byte i = 0; i < 3; i++) {//no need to flip yaw
+    for (byte i = 0; i < 3; i++) {  //no need to flip yaw
       ypr[i] *= degPerRad;
 #ifdef BiBoard
       ypr[i] = -ypr[i];
@@ -261,7 +261,7 @@ bool read_IMU() {
     }
     if (printGyro)
       print6Axis();
-    exceptions = aaReal.z < 0 && fabs(ypr[2]) > 85; //the second condition is used to filter out some noise
+    exceptions = aaReal.z < 0 && fabs(ypr[2]) > 85;  //the second condition is used to filter out some noise
     return true;
   }
   return false;
@@ -275,7 +275,7 @@ void imuSetup() {
   // join I2C bus (I2Cdev library doesn't do this automatically)
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
   Wire.begin();
-  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  Wire.setClock(400000);  // 400kHz I2C clock. Comment this line if having compilation difficulties
 #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
   Fastwire::setup(400, true);
 #endif
@@ -283,8 +283,9 @@ void imuSetup() {
   // initialize serial communication
   // (115200 chosen because it is required for Teapot Demo output, but it's
   // really up to you depending on your project)
-  Serial.begin(115200);
-  while (!Serial); // wait for Leonardo enumeration, others continue immediately
+  // Serial.begin(115200);
+  // while (!Serial)
+  // ;  // wait for Leonardo enumeration, others continue immediately
 
   // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
   // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
@@ -296,6 +297,11 @@ void imuSetup() {
     delay(500);
     // initialize device
     Serial.println(F("Initializing MPU..."));
+#if defined CONFIG_DISABLE_HAL_LOCKS && CONFIG_DISABLE_HAL_LOCKS == 1
+    Serial.println("OK");
+#else
+    Serial.println("If the program stucks, modify the header file:\n  https://docs.petoi.com/arduino-ide/upload-sketch-for-biboard#sdkconfig.h");
+#endif
     mpu.initialize();
     pinMode(INTERRUPT_PIN, INPUT);
     // verify connection
@@ -307,6 +313,7 @@ void imuSetup() {
 
   // load and configure the DMP
   Serial.println(F("- Initializing DMP..."));
+
   devStatus = mpu.dmpInitialize();
 
   for (byte m = 0; m < 6; m++)
