c4c30544b8ff50d6df9ceab410f2343941c370af
Jason.W <jarodwang1981@gmail.com>
2025-03-19 17:06:54 +0800

    Update the version date

--- src/OpenCat.h
+++ src/OpenCat.h
@@ -76,7 +76,7 @@
 #else
 #define BOARD "B"
 #endif
-#define DATE "250306"  // YYMMDD
+#define DATE "250319"  // YYMMDD
 String SoftwareVersion = "";
 
 #define BIRTHMARK '@'  // Send '!' token to reset the birthmark in the EEPROM so that the robot will know to restart and reset
